91dc8caabb4658d298d3d6baf6ad09747a222f60
LIvanoff <drfrily@gmail.com>
2024-04-03 00:00:50 +0300

    обновил model.py

--- src/model.py
+++ src/model.py
@@ -13,8 +13,8 @@ class Model:
         self.topk = topk
         self.model_name = self.config.NAME
         self.model = SentenceTransformer(self.model_name)
-        self.db_name = self.config.EMBEDDING_NAME
-        self.embedding_name = self.config.EMBEDDING_NAME
+        self.db_name = self.config.OUTPUT
+        self.output_name = self.config.OUTPUT
 
     def vectorize_img(self, img_path: str) -> np.array:
         img = Image.open(img_path)
@@ -37,13 +37,13 @@ class Model:
             if os.path.isfile(image_path):
                 emb = self.vectorize_img(image_path)
                 data_dict[file_name] = emb
-        images_db = pd.DataFrame(data_dict.items(), columns=['Image', self.embedding_name])
+        images_db = pd.DataFrame(data_dict.items(), columns=['Image', self.output_name])
         images_db.to_excel(f"{self.db_name}.xlsx")
         return images_db
 
     def get_df(self, df_path: str) -> pd.DataFrame:
         data_df = pd.read_json(df_path)
-        data_df[f'{self.embedding_name}'] = data_df[f'{self.embedding_name}'].apply(lambda x: np.array(x))
+        data_df[f'{self.output_name}'] = data_df[f'{self.output_name}'].apply(lambda x: np.array(x))
         return data_df
 
     @staticmethod
@@ -51,22 +51,10 @@ class Model:
         result_distance = spatial.distance.cosine(emb_a, emb_b)
         return result_distance
 
-    '''надо переписать под разные модели'''
-    def found_similar_images(self, input_img_path: str, images_db: pd.DataFrame) -> pd.DataFrame:
-        input_vec = self.vectorize_img(input_img_path)
-        result_df = copy.deepcopy(images_db)
-        import time
-        time_start = time.time()
-        result_df['Distance_with_input'] = result_df.apply(lambda x: self.calculate_cos_dist(input_vec, x[f'{self.embedding_name}']), axis=1)
-        print(time.time() - time_start)
-        result_df_sorted = result_df.sort_values('Distance_with_input').reset_index()
-        result_df_sorted = result_df_sorted[['Image', 'Distance_with_input']]
-        return result_df_sorted.head(self.topk)
-
     def __call__(self, input, *args, **kwargs):
-        if self.embedding_name == 'vec_img_text':
+        if self.output_name == 'vec_img_text':
             output = self.vectorize_img(input)
-        elif self.embedding_name == 'vec_text':
+        elif self.output_name == 'vec_text':
             output = self.vectorize_img(input)
         else:
             output = self.auto_markup(input)
