cf438f5cb65de2c5569cabb442eee82b74f2c97a
Leonid <drfrily@gmail.com>
2024-04-07 13:32:14 +0300

    upd: inference.py

--- src/models/inference.py
+++ src/models/inference.py
@@ -41,10 +41,11 @@ class ModelLauncher:
         # image = cv2.imread(file)
         image = np.array(input)
         for res in output:
+            print(f'res: {res}')
             top_left = tuple(res[0][0])  # top left coordinates as tuple
             bottom_right = tuple(res[0][2])  # bottom right coordinates as tuple
             # draw rectangle on image
-            image = cv2.rectangle(image, top_left, bottom_right, (0, 255, 0), 2)
+            image = cv2.rectangle(image, top_left, bottom_right, (255, 255, 255), 2)
             # write recognized text on image (top_left) minus 10 pixel on y
             image = cv2.putText(image, res[1], (top_left[0], top_left[1] - 10), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
         # cv2.imshow(image)
@@ -117,7 +118,11 @@ class ModelLauncher:
         return output
 
     def translate(self, input):
-        output = self.model(input)
+        output = self.model(
+                            input,
+                            paragraph=self.paragraph,
+                            text_threshold=self.text_threshold
+                            )
         return output
 
 # model = ModelLauncher('image_text_encoding')
