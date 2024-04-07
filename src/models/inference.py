5c46c7cff51c2163730c9f7e4a9486438fe6fe94
Leonid <drfrily@gmail.com>
2024-04-07 11:59:13 +0300

    upd: inference.py

--- src/models/inference.py
+++ src/models/inference.py
@@ -44,12 +44,10 @@ class ModelLauncher:
             top_left = tuple(res[0][0])  # top left coordinates as tuple
             bottom_right = tuple(res[0][2])  # bottom right coordinates as tuple
             # draw rectangle on image
-            print(top_left)
-            print(bottom_right)
-            cv2.rectangle(image, top_left, bottom_right, (0, 255, 0), 2)
+            image = cv2.rectangle(image, top_left, bottom_right, (0, 255, 0), 2)
             # write recognized text on image (top_left) minus 10 pixel on y
-            cv2.putText(image, res[1], (top_left[0], top_left[1] - 10), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
-        cv2.imshow(image)
+            image = cv2.putText(image, res[1], (top_left[0], top_left[1] - 10), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+        # cv2.imshow(image)
         return image
 
     def parse_config(self, config_path):
